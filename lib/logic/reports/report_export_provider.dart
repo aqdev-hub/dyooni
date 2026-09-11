254cdec4b9374375ce12910501cda9af37390172
AbdulQuddus Al-Shibani <sba849198@gmail.com>
2026-09-11 22:52:01 +0300

    Delete lib/logic directory

--- lib/logic/reports/report_export_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'pdf_report_service.dart';
-import 'xlsx_report_service.dart';
-
-final pdfReportServiceProvider = Provider<PdfReportService>((ref) => PdfReportService());
-final xlsxReportServiceProvider = Provider<XlsxReportService>((ref) => XlsxReportService());
